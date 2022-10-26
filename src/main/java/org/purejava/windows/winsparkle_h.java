340d8eb6caf1473b96b5b1e4af149620efc4284e
Ralph Plawetzki <ralph@purejava.org>
2022-10-26 18:30:30 +0200

    Revert "Use String instead of Addressable in method signature"
    This reverts commit 6733246668583b707510b154937d25fc4e9133ab.

--- src/main/java/org/purejava/windows/winsparkle_h.java
+++ src/main/java/org/purejava/windows/winsparkle_h.java
@@ -2,7 +2,6 @@
 
 package org.purejava.windows;
 
-import java.io.File;
 import java.lang.foreign.Addressable;
 import java.lang.foreign.MemoryAddress;
 import java.lang.foreign.MemoryLayout;
@@ -736,11 +735,7 @@ public class winsparkle_h  {
     public static MethodHandle win_sparkle_set_dsa_pub_pem$MH() {
         return RuntimeHelper.requireNonNull(constants$6.win_sparkle_set_dsa_pub_pem$MH,"win_sparkle_set_dsa_pub_pem");
     }
-    public static int win_sparkle_set_dsa_pub_pem ( String dsa_pub_pem) {
-        var file = new File(dsa_pub_pem);
-        if (!file.isFile()) {
-            throw new IllegalArgumentException("Unreadable file: " + dsa_pub_pem);
-        }
+    public static int win_sparkle_set_dsa_pub_pem ( Addressable dsa_pub_pem) {
         var mh$ = win_sparkle_set_dsa_pub_pem$MH();
         try {
             return (int)mh$.invokeExact(dsa_pub_pem);
