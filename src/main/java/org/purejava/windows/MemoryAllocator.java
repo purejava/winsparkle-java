13c188de3536463b61cf4b27673a59c982df5928
Ralph Plawetzki <ralph@purejava.org>
2022-11-01 15:36:30 +0100

    Allocate wchar_t for Strings

--- src/main/java/org/purejava/windows/MemoryAllocator.java
+++ src/main/java/org/purejava/windows/MemoryAllocator.java
@@ -1,9 +1,14 @@
 package org.purejava.windows;
 
 import java.lang.foreign.MemorySegment;
+import java.lang.foreign.ValueLayout;
 
 public class MemoryAllocator {
     public static MemorySegment ALLOCATE_FOR(String str) {
         return RuntimeHelper.CONSTANT_ALLOCATOR.allocateUtf8String(str);
+    }
+
+    public static MemorySegment ALLOCATE_FOR_WCHAR_T(String str) {
+        return RuntimeHelper.CONSTANT_ALLOCATOR.allocateArray(ValueLayout.JAVA_CHAR, str.toCharArray());
     }
 }
