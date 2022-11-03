8170a2fede869148ae81a683dd1883fd87da6f9c
Ralph Plawetzki <ralph@purejava.org>
2022-11-03 17:45:31 +0100

    Fix line ending Rename method reference

--- src/main/java/org/purejava/windows/MemoryAllocator.java
+++ src/main/java/org/purejava/windows/MemoryAllocator.java
@@ -8,7 +8,8 @@ public class MemoryAllocator {
         return RuntimeHelper.CONSTANT_ALLOCATOR.allocateUtf8String(str);
     }
 
-    public static MemorySegment ALLOCATE_FOR_WCHAR_T(String str) {
+    public static MemorySegment ALLOCATE_WCHAR_T_FOR(String str) {
+        str += "\0"; // The \0 character marks the end of the string stored in a char array
         return RuntimeHelper.CONSTANT_ALLOCATOR.allocateArray(ValueLayout.JAVA_CHAR, str.toCharArray());
     }
 }
