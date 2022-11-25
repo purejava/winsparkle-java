4db77f991ffb5312e61b1342b0b6118866d7fd87
Ralph Plawetzki <ralph@purejava.org>
2022-11-25 19:30:39 +0100

    Add code for callback

--- src/main/java/org/purejava/windows/MemoryAllocator.java
+++ src/main/java/org/purejava/windows/MemoryAllocator.java
@@ -1,6 +1,7 @@
 package org.purejava.windows;
 
 import java.lang.foreign.MemorySegment;
+import java.lang.foreign.MemorySession;
 import java.lang.foreign.ValueLayout;
 
 public class MemoryAllocator {
@@ -11,5 +12,9 @@ public class MemoryAllocator {
     public static MemorySegment ALLOCATE_WCHAR_T_FOR(String str) {
         str += "\0"; // The \0 character marks the end of the string stored in a char array
         return RuntimeHelper.CONSTANT_ALLOCATOR.allocateArray(ValueLayout.JAVA_CHAR, str.toCharArray());
+    }
+
+    public static MemorySegment ALLOCATE_CALLBACK_FOR(win_sparkle_did_find_update_callback_t cb) {
+        return win_sparkle_did_find_update_callback_t.allocate(cb, MemorySession.openImplicit());
     }
 }
